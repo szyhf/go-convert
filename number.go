85f3d6d537d2c7a06c370b38050affa1b188e13e
Back Yu <yhfszb@gmail.com>
2017-03-01 08:36:44 +0800

    交换了int64

--- number.go
+++ number.go
@@ -417,7 +417,6 @@ func ToInt32(value interface{}) (res int32, err error) {
 			res, err = ToInt32(valueStr)
 		}
 	}
-
 	return
 }
 
@@ -427,12 +426,96 @@ func ToInt32(value interface{}) (res int32, err error) {
 // uint8~64、int8~64都会做默认的转换
 // bool类型的数据，true-1；false-0
 func ToInt64(value interface{}) (res int64, err error) {
-	defer func() {
-		if r := recover(); r != nil {
-			err = fmt.Errorf("%v", r)
+	switch value.(type) {
+	case string:
+		{
+			valueString := value.(string)
+			if len(valueString) == 0 {
+				return 0, nil
+			}
+			//string的情况比较复杂，需要继续区分string是bool、float、int、uint再处理
+			if res64, erro := strconv.ParseInt(valueString, 0, 0); erro != nil {
+				if resU64, erro := strconv.ParseUint(valueString, 0, 0); erro != nil {
+					if resF64, erro := strconv.ParseFloat(valueString, 0); erro != nil {
+						if resBool, erro := strconv.ParseBool(valueString); erro != nil {
+							err = fmt.Errorf("Convert string \"%s\" to int64 failed", value.(string))
+						} else {
+							res, err = ToInt64(resBool)
+						}
+					} else {
+						res, err = ToInt64(resF64)
+					}
+				} else {
+					res, err = ToInt64(resU64)
+				}
+
+			} else {
+				res = int64(res64)
+			}
 		}
-	}()
-	res = MustInt64(value)
+	case int:
+		{
+			res = int64(value.(int))
+		}
+	case int8:
+		{
+			res = int64(value.(int8))
+		}
+	case int16:
+		{
+			res = int64(value.(int16))
+		}
+	case int32:
+		{
+			res = int64(value.(int64))
+		}
+	case uint:
+		{
+			res = int64(value.(uint))
+		}
+	case uint8:
+		{
+			res = int64(value.(uint8))
+		}
+	case uint16:
+		{
+			res = int64(value.(uint16))
+		}
+	case uint32:
+		{
+			res = int64(value.(uint32))
+		}
+	case uint64:
+		{
+			res = int64(value.(uint64))
+		}
+	case uintptr:
+		{
+			res = int64(value.(uintptr))
+		}
+
+	case float32:
+		{
+			res = int64(value.(float32))
+		}
+	case float64:
+		{
+			res = int64(value.(float64))
+		}
+	case bool:
+		{
+			if value.(bool) {
+				res = 1
+			} else {
+				res = 0
+			}
+		}
+	default:
+		{
+			valueStr := MustString(value)
+			res, err = ToInt64(valueStr)
+		}
+	}
 	return
 }
 
@@ -814,110 +897,6 @@ func MustUint64Array(value interface{}) (resArray []uint64) {
 		}
 	}
 	panic("Not an array or slice")
-	return
-}
-
-// 强制转换为int64，失败则panic
-// string会按顺序尝试将数据解析为int64\uint64\float64\bool，然后再转换为int64
-// float会抹去小数
-// uint8~64、int8~64都会做默认的转换
-// bool类型的数据，true-1；false-0
-func MustInt64(value interface{}) (res int64) {
-	var err error
-	switch value.(type) {
-	case string:
-		{
-			valueString := value.(string)
-			if len(valueString) == 0 {
-				return 0
-			}
-			//string的情况比较复杂，需要继续区分string是bool、float、int、uint再处理
-			if res64, erro := strconv.ParseInt(valueString, 0, 0); erro != nil {
-				if resU64, erro := strconv.ParseUint(valueString, 0, 0); erro != nil {
-					if resF64, erro := strconv.ParseFloat(valueString, 0); erro != nil {
-						if resBool, erro := strconv.ParseBool(valueString); erro != nil {
-							err = fmt.Errorf("Convert string \"%s\" to int64 failed", value.(string))
-						} else {
-							res = MustInt64(resBool)
-						}
-					} else {
-						res = MustInt64(resF64)
-					}
-				} else {
-					res = MustInt64(resU64)
-				}
-
-			} else {
-				res = int64(res64)
-			}
-		}
-	case int:
-		{
-			res = int64(value.(int))
-		}
-	case int8:
-		{
-			res = int64(value.(int8))
-		}
-	case int16:
-		{
-			res = int64(value.(int16))
-		}
-	case int32:
-		{
-			res = int64(value.(int64))
-		}
-	case uint:
-		{
-			res = int64(value.(uint))
-		}
-	case uint8:
-		{
-			res = int64(value.(uint8))
-		}
-	case uint16:
-		{
-			res = int64(value.(uint16))
-		}
-	case uint32:
-		{
-			res = int64(value.(uint32))
-		}
-	case uint64:
-		{
-			res = int64(value.(uint64))
-		}
-	case uintptr:
-		{
-			res = int64(value.(uintptr))
-		}
-
-	case float32:
-		{
-			res = int64(value.(float32))
-		}
-	case float64:
-		{
-			res = int64(value.(float64))
-		}
-	case bool:
-		{
-			if value.(bool) {
-				res = 1
-			} else {
-				res = 0
-			}
-		}
-	default:
-		{
-			valueStr := MustString(value)
-			res, err = ToInt64(valueStr)
-		}
-	}
-
-	if err != nil {
-		panic(err)
-	}
 	return
 }
 
--- number_must.go
+++ number_must.go
@@ -51,3 +51,16 @@ func MustInt32(value interface{}) (res int32) {
 		return res
 	}
 }
+
+// 强制转换为int64，失败则panic
+// string会按顺序尝试将数据解析为int64\uint64\float64\bool，然后再转换为int64
+// float会抹去小数
+// uint8~64、int8~64都会做默认的转换
+// bool类型的数据，true-1；false-0
+func MustInt64(value interface{}) (res int64) {
+	if res, err := ToInt64(value); err != nil {
+		panic(err)
+	} else {
+		return res
+	}
+}
