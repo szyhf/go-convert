4df3e6ca5138b7114f9b7c4359d6629c38aafd48
Back Yu <yhfszb@gmail.com>
2017-03-10 14:31:15 +0800

    修理switch的bug

--- string.go
+++ string.go
@@ -20,8 +20,7 @@ func ToString(value interface{}) (string, error) {
 func MustStringArray(array interface{}) (resArray []string) {
 	t := reflect.TypeOf(array)
 	switch t.Kind() {
-	case reflect.Array:
-	case reflect.Slice:
+	case reflect.Array, reflect.Slice:
 		{
 			v := reflect.ValueOf(array)
 			resArray = make([]string, v.Len())
